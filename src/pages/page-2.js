8a7ae17fe8648a61640fefc0490985023b0c4c56
Stephen Parker <sparker888@gmail.com>
2022-06-21 10:41:59 -0400

    recolor form page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,12 +6,17 @@ import Seo from "../components/seo"
 import Waves from "../components/birds"
 
 const SecondPage = () => (
-  <div className="h-full bg-sky-400">
-    <Waves className="absolute h-1000">
+  <main
+            className="h-screen bg-cover bg-top sm:bg-top"
+            style={{
+              backgroundImage:
+                'url("https://res.cloudinary.com/gravital-digital/image/upload/v1655821187/mesh-gradient-green-brown-cropped_eu9n4k.jpg")',
+            }}
+          >
       <div class="grid place-items-center h-screen">
         <div class="w-full md:w-96 md:max-w-full mx-auto">
-          <div className="p-6 border border-gray-600 sm:rounded-md bg-gradient-to-r from-sky-500 to-indigo-500">
-          <h3 className="mb-6 text-2xl font-extrabold tracking-tight text-violet-800 sm:text-3xl">
+          <div className="p-6 border border-gray-600 sm:rounded-md bg-orange-200">
+          <h3 className="mb-6 text-2xl font-extrabold tracking-tight text-black text-opacity-50 sm:text-3xl">
                 Contact Robin!
               </h3>
             <form name="contact"
@@ -27,7 +32,7 @@ const SecondPage = () => (
                   </label>
                 </p>
               <label className="block mb-6">
-                <span className="text-indigo-700">name:</span>
+                <span className="text-black text-opacity-75">name:</span>
                 <input
                   name="name"
                   type="text"
@@ -44,12 +49,13 @@ const SecondPage = () => (
             focus:ring-opacity-50
             
             placeholder-gray-600
-            text-indigo-700
+            text-black text-opacity-75
+            focus:outline-none
           "
                 />
               </label>
               <label className="block mb-6">
-                <span className="text-indigo-700">email:</span>
+                <span className="text-black text-opacity-75">email:</span>
                 <input
                   name="email"
                   type="email"
@@ -66,13 +72,14 @@ const SecondPage = () => (
             focus:ring-opacity-50
             
             placeholder-gray-600
-            text-indigo-700
+            text-black text-opacity-75
+            focus:outline-none
           "
                   required
                 />
               </label>
               <label className="block mb-6">
-                <span className="text-indigo-700">message:</span>
+                <span className="text-black text-opacity-75">message:</span>
                 <textarea
                   name="message"
                   className="
@@ -88,7 +95,8 @@ const SecondPage = () => (
             focus:ring-opacity-50
             
             placeholder-gray-600
-            text-indigo-700
+            text-black text-opacity-75
+            focus:outline-none
           "
                   rows="3"
                 ></textarea>
@@ -99,20 +107,20 @@ const SecondPage = () => (
                   className="
             h-10
             px-5
-            text-indigo-100
-            bg-indigo-700
+            text-black text-opacity-75
+            bg-orange-400
             rounded-lg
             transition-colors
             duration-150
             focus:shadow-outline
-            hover:bg-indigo-800
+            hover:bg-orange-500
           "
                 >
                   Send it...
                 </button>
               </div>
               <div>
-                <div className="mt-2 text-gray-300 text-right text-xs">
+                <div className="mt-2 text-black text-opacity-75 text-right text-xs">
                   <a
                     href="/"
                     className="hover:underline"
@@ -125,8 +133,7 @@ const SecondPage = () => (
           </div>
         </div>
       </div>
-    </Waves>
-  </div>
+  </main>
 )
 
 export default SecondPage
